435eaeed612b0a5131baaff86568b57b37fd287e
Maksymilian Rzeczycki <maks.rzeczycki@gmail.com>
2024-11-02 19:40:21 +0100

    Taking input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String className = args[0];
+        List<String> jarPaths = new ArrayList<>();
+
+        for (int i = 1; i < args.length; i++) {
+            jarPaths.add(args[i]);
+        }
+
+
     }
 }
